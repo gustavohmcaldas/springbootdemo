aae1644e4ea134a227c5b67e519611ed4f3f5dfc
Gustavo Caldas <gustavohmcaldas@gmail.com>
2021-09-28 20:47:35 -0700

    Implementing Security

--- src/main/java/com/gustavohmcaldas/studentapi/config/StudentConfig.java
+++ src/main/java/com/gustavohmcaldas/studentapi/config/StudentConfig.java
@@ -1,7 +1,5 @@
 package com.gustavohmcaldas.studentapi.config;
 
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/gustavohmcaldas/studentapi/controller/StudentController.java
+++ src/main/java/com/gustavohmcaldas/studentapi/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,7 +53,7 @@ public class StudentController {
 		return new ResponseEntity<>(studentService.addStudent(studentPostRequestBody), HttpStatus.CREATED);
 	}
 	
-	@DeleteMapping(path = "/{studentId}")
+	@DeleteMapping(path = "/admin/{studentId}")
 	public ResponseEntity<Void> deleteStudent(@PathVariable("studentId") long id) {
 		studentService.deleteStudent(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
