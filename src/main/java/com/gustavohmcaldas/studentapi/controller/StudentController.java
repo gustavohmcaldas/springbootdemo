f695c0664c8002c4761563dec34a34af01fab7ab
Gustavo Caldas <gustavohmcaldas@gmail.com>
2021-09-28 23:07:37 -0700

    Implementing documentation with SpringDoc OpenAPI

--- src/main/java/com/gustavohmcaldas/studentapi/controller/StudentController.java
+++ src/main/java/com/gustavohmcaldas/studentapi/controller/StudentController.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +24,9 @@ import com.gustavohmcaldas.studentapi.requests.StudentPostRequestBody;
 import com.gustavohmcaldas.studentapi.requests.StudentPutRequestBody;
 import com.gustavohmcaldas.studentapi.service.StudentService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -34,32 +37,72 @@ public class StudentController {
 	private final StudentService studentService;
 	
 	@GetMapping
-	public ResponseEntity<Page<Student>> getStudents(Pageable pageable) {
+	@Operation(summary = "List all students (paged)", 
+				description = "The default size is 20, use the parameter size to change the default value",
+				tags = {"student"})
+	@ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Operation"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
+	public ResponseEntity<Page<Student>> getStudents(@ParameterObject Pageable pageable) {
 		return ResponseEntity.ok(studentService.getStudents(pageable));
 	}
 	
+	@Operation(summary = "Get student by id", tags = {"student"})
 	@GetMapping(path = "/{studentId}")
+	@ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Operation"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
 	public ResponseEntity<Student> findById(@PathVariable("studentId") long id) {
 		return ResponseEntity.ok(studentService.findById(id));
 	}
 	
 	@GetMapping(path = "/find")
+	@Operation(summary = "Get student by first name", tags = {"student"})
+	@ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Operation"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
 	public ResponseEntity<List<Student>> findByFirstName(@RequestParam String firstName) {
 		return ResponseEntity.ok(studentService.findByFirstName(firstName));
 	}
 	
 	@PostMapping
+	@Operation(summary = "Insert a new student", tags = {"student"})
+	@ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful Operation"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
 	public ResponseEntity<Student> addStudent(@RequestBody  @Valid StudentPostRequestBody studentPostRequestBody) {
 		return new ResponseEntity<>(studentService.addStudent(studentPostRequestBody), HttpStatus.CREATED);
 	}
 	
 	@DeleteMapping(path = "/admin/{studentId}")
+	@Operation(summary = "Remove student", 
+				description = "Restricted method with admin user only",
+				tags = {"student"})
+	@ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful Operation"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden")
+    })
 	public ResponseEntity<Void> deleteStudent(@PathVariable("studentId") long id) {
 		studentService.deleteStudent(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 	@PutMapping
+	@Operation(summary = "Update student", 
+				tags = {"student"})
+	@ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful Operation"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
 	public ResponseEntity<Void> updateStudent(@RequestBody @Valid StudentPutRequestBody studentPutRequestBody) {
 		studentService.updateStudent(studentPutRequestBody);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- src/main/java/com/gustavohmcaldas/studentapi/requests/StudentPutRequestBody.java
+++ src/main/java/com/gustavohmcaldas/studentapi/requests/StudentPutRequestBody.java
@@ -2,6 +2,7 @@ package com.gustavohmcaldas.studentapi.requests;
 
 import javax.validation.constraints.NotEmpty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,16 +10,21 @@ import lombok.Data;
 @Builder
 public class StudentPutRequestBody {
 	
+	@Schema(description = "This is the student's id", example = "1")
 	private Long id;
 	
+	@Schema(description = "This is the student's first name", example = "Mary", required = true)
 	@NotEmpty(message = "First name must not be empty.")
 	private String firstName;
 	
+	@Schema(description = "This is the student's last name", example = "Smith", required = true)
 	@NotEmpty(message = "Last name must not be empty.")
 	private String lastName;
 	
+	@Schema(description = "This is the student's email", example = "mary@test.com", required = true)
 	@NotEmpty(message = "Email must not be empty.")
 	private String email;
 	
+	@Schema(description = "This is the student's age", example = "33")
 	private Integer age;
 }
